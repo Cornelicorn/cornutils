bae1ffcd784b1de55ffbfb2a26abf7451f597b58
Cornelius Hoffmann <coding@hoffmn.de>
2020-11-08 21:36:56 +0100

    Use dataclass for experimental data

--- cornutils/praktika.py
+++ cornutils/praktika.py
@@ -44,6 +44,26 @@ class PlotSettings:
     loc_legend: str = 'lower right'
 
 
+@dataclass
+class Data:
+    """Dataclass for plot/regression data
+
+    Attributes
+    ----------
+    x: np.ndarray
+        x values
+    y: np.ndarray
+        y values
+    sx: np.ndarray, optional
+        Standard deviation in x. Defaults to None
+    sy: np.ndarray, optional
+        Standard deviation in y. Defaults to None
+    """
+    x: np.ndarray
+    y: np.ndarray
+    sx: np.ndarray = None
+    sy: np.ndarray = None
+
 def find_nearest_idx(array: np.ndarray,
                 value
                 ):
@@ -59,43 +79,37 @@ def estimation(x: np.ndarray,
 
 def regression(func: Callable,
                num_params: int,
-               x: np.ndarray,
-               y: np.ndarray,
-               sx: np.ndarray = None,
-               sy: np.ndarray = None,
+               data: Data
               ) -> Tuple[np.ndarray, np.ndarray, float, float]:
-    '''
-    Computes the Regression of func, given values `x` and `y`
-    and optional errors `sx` and `sy`. `func` should be
-    `func(beta, x) -> y` and beta an array of length
-    num_params. Returns `beta`, deviation of `beta` as well
+    '''Do an ODR
+    Computes the Regression of func, given values
+    and optional standard deviations and a function
+    model.
+    Returns `beta`, deviation of `beta` as well
     as reduced $\chi^2$ and $\R^2$
     '''
-    data = RealData(x, y, sx=sx, sy=sy)
+    realdata = RealData(data.x, data.y, sx=data.sx, sy=data.sy)
     model = Model(func)
-    odr = ODR(data, model, beta0 = estimation(x,y, num_params))
+    odr = ODR(realdata, model, beta0 = estimation(data.x,data.y, num_params))
     
     # If sx is given do odr, else just do least-squares
-    fit_type = 0 if isinstance(sx, np.ndarray) else 2
+    fit_type = 0 if isinstance(data.sx, np.ndarray) else 2
     odr.set_job(fit_type=fit_type)
     
     output = odr.run()
     
-    residuals = y - func(output.beta, x)
-    chi_arr =  residuals / sy
-    chi2_red = np.sum(chi_arr**2) / (len(x)-len(output.beta))
-    ybar = np.sum(y/(sy**2))/np.sum(1/sy**2)
-    r2 = 1 - np.sum(chi_arr**2)/np.sum(((y-ybar)/sy)**2)
+    residuals = data.y - func(output.beta, data.x)
+    chi_arr =  residuals / data.sy
+    chi2_red = np.sum(chi_arr**2) / (len(data.x)-len(output.beta))
+    ybar = np.sum(data.y/(data.sy**2))/np.sum(1/data.sy**2)
+    r2 = 1 - np.sum(chi_arr**2)/np.sum(((data.y-ybar)/data.sy)**2)
     
-    return output.beta, output.sd_beta*np.sqrt(len(x)), chi2_red, r2
+    return output.beta, output.sd_beta*np.sqrt(len(data.x)), chi2_red, r2
 
 
 def plot(regression_erg: Tuple[np.ndarray, np.ndarray],
          func: Callable,
-         x: np.ndarray,
-         y: np.ndarray,
-         sx: np.ndarray = None,
-         sy: np.ndarray = None,
+         data: Data,
          s: PlotSettings = PlotSettings(),
         ):
     """Quickly plot data and ODR regression
@@ -121,14 +135,14 @@ def plot(regression_erg: Tuple[np.ndarray, np.ndarray],
     """
     fig = plt.figure(figsize = (10,6))
     
-    plt.errorbar(x, y, fmt='rx',
+    plt.errorbar(data.x, data.y, fmt='rx',
                  label=s.label_data,
-                 xerr=sx, yerr=sy, ecolor='black',
+                 xerr=data.sx, yerr=data.sy, ecolor='black',
                  dash_capstyle='butt', capsize=3,
                  ls = s.connect)
 
-    xmin = np.amin(x)
-    xmax = np.amax(x)
+    xmin = np.amin(data.x)
+    xmax = np.amax(data.x)
     t = np.arange(xmin, xmax, (xmax-xmin)/2000)
     
     plt.plot(t, func(regression_erg[0],t), label = s.label_fit)
@@ -144,15 +158,12 @@ def plot(regression_erg: Tuple[np.ndarray, np.ndarray],
 
 def aio(func: Callable,
         num_params: int,
-        x: np.ndarray,
-        y: np.ndarray,
-        sx: np.ndarray = None,
-        sy: np.ndarray = None,
+        data: Data
        ):
     '''
     Computes ODR/LS and prints output of regression 
     '''
-    regression_erg = regression(func, num_params, x, y, sx=sx, sy=sy)
+    regression_erg = regression(func, num_params, data)
     print(f'''
     beta: \t {regression_erg[0]}
     sbeta: \t {regression_erg[1]}
@@ -160,4 +171,4 @@ def aio(func: Callable,
     r2: \t {regression_erg[3]}
     '''
     )
-    plot(regression_erg, func, x, y ,sx=sx, sy=sy)
+    plot(regression_erg, func, data)
